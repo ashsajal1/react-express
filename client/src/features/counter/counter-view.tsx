0e33c141bd6524cb31583bf713ec205a4b6edf0d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-19 00:51:22 +0800

    Use all reducer in button

--- client/src/features/counter/counter-view.tsx
+++ client/src/features/counter/counter-view.tsx
@@ -1,13 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './counterSlice';
+import { decrement, increment, reset } from './counterSlice';
 
 export default function CounterView() {
     const count = useSelector(state => state.counter.count);
-    const dispathch = useDispatch()
-  return (
-    <div>
-        <p>Count : {count}</p>
-        <button className='p-1 border rounded' onClick={() => {dispathch(increment())}}>Increment</button>
-    </div>
-  )
+    const dispatch = useDispatch()
+    return (
+        <div className='p-2'>
+            <p>Count : {count}</p>
+            <div className='flex items-center gap-2'>
+            <button className='p-1 border rounded' onClick={() => { dispatch(increment()) }}>Increment</button>
+            <button className='p-1 border rounded' onClick={() => { dispatch(decrement()) }}>Decrement</button>
+            <button className='p-1 border rounded' onClick={() => { dispatch(reset()) }}>Reset</button>
+            </div>
+        </div>
+    )
 }
