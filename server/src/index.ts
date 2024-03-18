14c95c1c549ed2ade9bb0fcc8187c4f121402ec4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-19 06:47:58 +0800

    Add login and protected route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,43 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
+import authenticateToken from "./middleware";
 
 const app = express();
 
 app.use(bodyParser.json());
 
+// Dummy array of users (for demonstration purposes)
+const users = [
+  { id: 1, username: "alice", password: "secret" },
+  { id: 2, username: "bob", password: "password" },
+];
+
+app.post("/login", (req: Request, res: Response) => {
+  const { username, passowrd } = req.body;
+
+  const user = users.find((u) => u.username === username);
+
+  if (!user || user.password !== passowrd) {
+    return res.status(401).json({ message: "Invalid credentials!" });
+  }
+
+  const token = jwt.sign({ id: user.id }, "secret", { expiresIn: "1h" });
+
+  return res.json(token);
+});
 
 app.get("/", (req, res) => {
   res.json({
     ok: true,
     message: "Working fine",
+  });
+});
+
+app.get("/users", authenticateToken, (req, res) => {
+  res.json({
+    ok: true,
+    users,
   });
 });
 
