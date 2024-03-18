6f1f5f68bbdb9b103d1426c7c6862f617ab4f535
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-19 06:36:27 +0800

    add @types/jsonwebtoke

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.json({
     ok: true,
     message: "Working fine",
   });
 });
+
 app.listen(8080, () => {
   console.log(`Running at localhost:8080`);
 });
