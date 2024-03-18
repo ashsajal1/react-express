6ae704b9f0854a7175cd5524ef7eb7e1b90cacd8
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-19 06:35:02 +0800

    add body-parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json())
 app.get("/", (req, res) => {
   res.json({
     ok: true,
